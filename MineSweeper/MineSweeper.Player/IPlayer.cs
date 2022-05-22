c6a447dc50c7a78eb198e7b86a4e7df80507023d
greg <97074171+greg-clo@users.noreply.github.com>
2022-05-22 19:40:04 +0900

    add Player logic.(WIP)

---
+++ MineSweeper/MineSweeper.Player/IPlayer.cs
@@ -0,0 +1,28 @@
+﻿namespace MineSweeper.Player;
+
+public interface IPlayer
+{
+    void Initialize(int myNumber, int column, int row);
+
+    /// <summary>
+    /// 현재 턴의 동작을 결정한다.
+    /// </summary>
+    /// <param name="board">배치 현황. 전체 배치 정보가 1차원 배열로 할당되어 전달된다. 자세한 정보는 아래 참조.</param>
+    /// <param name="turnCount">현재 턴. 턴은 1부터 시작한다.</param>
+    /// <param name="myScore">현재 턴 이전까지 자신의 점수.</param>
+    /// <returns>현재 턴 동작.</returns>
+    PlayContext Turn(int[] board, int turnCount, int myScore);
+
+    /*
+    int[] borad 내용
+        1. 현재 보드의 전체 box 를 1차원 배열로 전달한다.
+            int 값으로 구성된다.
+            0 부터 시작한다.
+        2. 배열 요소
+            0 >= item : 해당 box 주변에 배치된 mime 의 개수.
+            0 < item : box state
+            -1 : unopened
+            -2 : mine
+            -3 : mark
+    */
+}
---
+++ MineSweeper/NXP-Mimesweeper/Player.cs
@@ -0,0 +1,28 @@
+﻿using MineSweeper.Player;
+
+namespace NXP.Mimesweeper;
+
+public class Player : IPlayer
+{
+    // sample!
+
+    private int _myNumber;
+
+    private int _column;
+
+    private int _row;
+
+    public void Initialize(int myNumber, int column, int row)
+    {
+        _myNumber = myNumber;
+        _column = column;
+        _row = row;
+    }
+
+    public PlayContext Turn(int[] board, int turnCount, int myScore)
+    {
+        var action = (PlayerAction)(new Random().Next(0, 2));
+        var position = new Random().Next(0, board.Length - 1);
+        return new PlayContext(action, position);
+    }
+}
